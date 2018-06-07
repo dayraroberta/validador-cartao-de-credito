a0f61615c9a78608ee824597e275fa1c2b8688e2
Dayra Roberta <dayraroberta@icloud.com>
2018-06-07 14:59:38 -0300

    Criei o loop e a condição final

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ for (var i = 0; i < 1; i = i){
 }
 var numberValidation = cardNumber.split('');
 numberValidation = numberValidation.reverse();
+
+var luhnCheck = 0;
+  for(var j = 0; j < numberValidation.length; j++){
+    luhnCheck = luhnCheck + parseInt(numberValidation[j]);
+    if((numberValidation[j + 1] * 2) > 9){
+      luhnCheck = luhnCheck + (numberValidation[j + 1] * 2 - 9);
+      j++;
+    } else{
+      luhnCheck = luhnCheck + (numberValidation[j + 1] * 2);
+      j++;
+    }
+  }
+  if(luhnCheck % 10 === 0){
+  document.getElementById('valid').innerHTML = 'Oba! O cartão de crédito inserido é valido.'
+  } else {
+  document.getElementById('notValid').innerHTML = 'Eita. O cartão de crédito inserido não é válido.'
+  }
+}
+isValidCard();
